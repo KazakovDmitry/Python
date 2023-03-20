7a854eddb33706b96ab6a7393345d1322c225ac0
Dmitry <dimakameya@gmail.com>
2023-03-20 21:40:31 +0500

    доп задачи

--- Seminars/sem3/dop2.py
+++ Seminars/sem3/dop2.py
@@ -25,3 +25,20 @@ Sample Input 3:
 ООООРРРРОРОРРРРРРРРООРОРОРРРРРРРРРРРРРРРРРРРРРРРРРРРРРРР
 Sample Output 3:
 31
+
+string_1 = input("Введите строку, состоящую из Р и О: ")
+string_1 = string_1.upper()
+i = 0
+sum = 0
+max = 0
+
+while i < len(string_1):
+    if (string_1[i] == 'Р'):
+        sum = sum + 1
+        if max<sum:
+            max=sum
+    else:
+        sum = 0
+    i = i + 1
+
+print(max)
