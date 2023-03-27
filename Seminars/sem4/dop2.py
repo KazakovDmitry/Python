13b50bd7e740c7768ea56751f4bc3822e8963786
Dmitry <dimakameya@gmail.com>
2023-03-27 18:12:43 +0500

    Доп задачи семинара 4

--- Seminars/sem4/dop2.py
+++ Seminars/sem4/dop2.py
@@ -5,3 +5,14 @@
 # *Пример: *
 # - [2, 3, 4, 5, 6] = > [12, 15, 16];
 # - [2, 3, 5, 6] = > [12, 15]
+
+# list1 = list(input("Введите числа через пробел: ").split())
+list1 = input("Введите числа через пробел: ").split()
+for i in range(len(list1)):
+    list1[i] = int(list1[i])
+print(list1, end=' ')
+list2 = list()
+for i in range((len(list1) + 1) // 2):
+    list2.append(list1[i] * list1[-i-1])
+print('= >', list2)
+
