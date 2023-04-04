a589f8981ba8dad738f2e8891147c7d0195e8c40
Dmitry <dimakameya@gmail.com>
2023-04-04 23:51:25 +0500

    sem8

--- DZ/DZ7/Task36.py
+++ DZ/DZ7/Task36.py
@@ -16,11 +16,11 @@
 
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    list1 = []
+
     for i in range(1, num_columns + 1):
         print()
         for j in range(1, num_rows + 1):
             print(operation(i, j), end='\t')
 
-print_operation_table(lambda x, y: x * y) 
+print_operation_table(lambda x, y: x * y)
 
